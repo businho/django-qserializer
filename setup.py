94e9ce2725e4174da983a990cfcfd9ab721b159c
Mateus Oliveira <66965232+mateusoliveira43@users.noreply.github.com>
2022-11-11 16:19:12 -0300

    fix: Fix flake8 dependency error (#5)
    * fix: Fix flake8 dependency error
    
    * fixup! fix: Fix flake8 dependency error

--- setup.py
+++ setup.py
@@ -1,5 +1,13 @@
 from setuptools import find_packages, setup
 
+test_requirements = [
+    'pytest',
+    'pytest-cov',
+    'pytest-django',
+    'pytest-flake8',
+    'flake8<5',
+]
+
 setup(
     packages=find_packages(),
     include_package_data=True,
@@ -9,10 +17,8 @@ setup(
     setup_requires=[
         'pytest-runner',
     ],
-    tests_require=[
-        'pytest',
-        'pytest-cov',
-        'pytest-django',
-        'pytest-flake8',
-    ],
+    tests_require=test_requirements,
+    extras_require={
+        'test': test_requirements,
+    },
 )
